d7d46a7281679c11b663b98fc0fdf030ab688bc4
Bob Ziroll <elderziroll@gmail.com>
2019-12-09 16:57:02 -0700

    Added support for button styles, started working on sizes

--- src/components/Button/utils.js
+++ src/components/Button/utils.js
@@ -2,19 +2,49 @@ import * as colors from "../../colors"
 
 export function determineButtonColors(props) {
     let backgroundColor, color, borderColor
-    switch (props.color) {
-        case "gray":
+    switch (props.buttonStyle) {
+        case "primary-dark":
         default:
             borderColor = colors.black
-            if (props.inverse) {
-                backgroundColor = colors.white
-                color = colors.gray.darker
-            } else {
-                backgroundColor = colors.gray.darker
-                color = colors.white
-            }
+            backgroundColor = colors.gray.darker
+            color = colors.white
+            break
+        case "primary-light":
+            borderColor = colors.black
+            backgroundColor = colors.gray.lighter
+            color = colors.black
+            break
+        case "secondary-dark":
+            borderColor = colors.black
+            backgroundColor = colors.gray.lighter
+            color = colors.black
+            break
+        case "secondary-light":
+            borderColor = colors.white
+            backgroundColor = "transparent"
+            color = colors.white
             break
     }
 
     return { backgroundColor, color, borderColor }
+}
+
+export function determineButtonSize(props) {
+    let height
+    switch (props.buttonSize) {
+        case "sm":
+            height = "32px"
+            break
+        case "md":
+            height = "40px"
+            break
+        case "lg":
+        default:
+            height = "48px"
+            break
+        case "xl":
+            height = "56px"
+            break
+    }
+    return { height }
 }
