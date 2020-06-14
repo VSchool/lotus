74800c0a64c19490f0442c7fa8a4714608a04798
Michael Stedman <mikeybones@rocketmail.com>
2020-06-14 17:44:36 -0500

    Commit
    Pushing to branch for review.

---
+++ example/src/example-components/Card.example.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import { Card } from '@vschool/lotus'
+
+export default () => {
+    return (
+        <Fragment>
+            <h1>Cards</h1>
+            <Card />
+        </Fragment>
+    )
+}
