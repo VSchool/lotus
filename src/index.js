af65c47569eda715a319198f154468dcc2fe1610
Michael Stedman <mikeybones@rocketmail.com>
2020-06-21 16:16:31 -0500

    Commit
    Commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { StatusChip } from "./components/StatusChip"
 export { StatusMessage } from './components/StatusMessage'
 export { Card } from './components/Card'
 export { Modal } from './components/Modal'
+export { Footer } from './components/Footer'
 export {
     blue,
     green,
