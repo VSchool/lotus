219e0a85687a5e3f2d80605794b26c8678aa4b5c
Michael Stedman <mikeybones@rocketmail.com>
2020-06-23 13:13:46 -0500

    Header Component Styled
    Header component is styled and mobile at 768px.

---
+++ example/src/example-components/Header.example.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import { Header } from '@vschool/lotus'
+
+export default () => {
+    return (
+        <Fragment>
+            <br></br>
+            <h1>Header</h1>
+            <Header />
+            <br></br>
+            <Header />
+            <br></br>
+            <br></br>
+
+        </Fragment>
+    )
+}
