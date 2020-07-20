7c66a942d159bba937ca4c5c7fd0cd42fda21ef1
Michael Stedman <mikeybones@rocketmail.com>
2020-07-20 18:57:14 -0500

    Undoing some things

--- example/src/components/Table.js
+++ example/src/components/Table.js
@@ -2,13 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TableContainer = styled.div`
-    border: 1px solid green;
+    /* border: 1px solid green; */
 `
+
+const PropTable = styled.table`
+
+    width: 100%;
+    height: 200px;
+    /* border: 1px solid gold; */
+`
+
+const TableRow = styled.div`
+    height: 24px;
+    border: 1px dotted gray;
+`
+
 
 export default function Table() {
     return (
         <TableContainer>
+            <PropTable>
+                <TableRow>
 
+                </TableRow>
+            </PropTable>
         </TableContainer>
     )
 }
