878e638f414811364ff3128a867b90af85684357
Michael Stedman <mikeybones@rocketmail.com>
2020-07-20 11:41:38 -0500

    Changing layout
    Still working to fill things in.

---
+++ example/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+    width: 100%;
+    background: white;
+`
+
+export default function Main() {
+    return (
+        <MainContainer>
+            Main
+        </MainContainer>
+    )
+}
---
+++ example/src/components/SideNav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SideNavContainer = styled.div`
+    width: 240px;
+    height: 1024px;
+    background: #C4C4C4;
+`
+
+export default function SideNav() {
+    return (
+        <SideNavContainer>
+            Nav
+        </SideNavContainer>
+    )
+}
