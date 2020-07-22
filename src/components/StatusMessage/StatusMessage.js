be720a5b01ecd029c5a542699470ec85c2cb6b2d
Michael Stedman <mikeybones@rocketmail.com>
2020-07-22 12:24:02 -0500

    Updated StatusMessage.js
    Restyling.

--- src/components/StatusMessage/StatusMessage.js
+++ src/components/StatusMessage/StatusMessage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { setStatus } from './utils'
 // import PropTypes from 'prop-types'
 
 // import { setStatus } from './utils'
@@ -10,13 +11,78 @@ const StatusMessageContainer = styled.div`
     position: relative;
     width: 378px;
     height: 128px;
+    display: flex;
+    align-items: center;
+    background: #E3E8FA;
+    border: 2px solid #859FEB;
+    /* box-sizing: border-box; */
     box-shadow: 0px 7px 8px rgba(0, 0, 0, 0.2), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 12px 17px rgba(0, 0, 0, 0.14);
+    /* border: 1px solid lightcoral; */
+    /* @media (min-width: 768px) {
+        max-width: 1024px;
+        height: 56px;
+    } */
 `
 
+const IndicatorContainer = styled.div`
+    min-width: 56px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #456AE2;
+    /* blue/primary */
+
+    border: 1px solid #456AE2;
+    /* box-sizing: border-box; */
+    /* border: 1px dotted gray; */
+`
+
+const Indicator = styled.p`
+    margin: 0px;
+    font-family: 'benton-sans';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 24px;
+    line-height: 29px;
+    /* identical to box height */
+
+    text-align: center;
+    color: #FFFFFF;
+`
+
+const MessageContainer = styled.div`
+    width: 100%;
+    min-height: 100%;
+    padding: 16px 16px 16px 16px;
+    box-sizing: border-box;
+    /* border: 1px solid lightcoral; */
+`
+
+const Message = styled.p`
+    margin: 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #21201F;
+    border: 1px solid green;
+`
+
+
 export default function StatusMessage() {
+
+    // console.log(setStatus('success'))
+
     return (
         <StatusMessageContainer>
-            helo
+            <IndicatorContainer>
+                <Indicator>{':)'}</Indicator>
+            </IndicatorContainer>
+            <MessageContainer>
+                <Message>Welcome to your V School Application. Get started by completing your Background Information</Message>
+            </MessageContainer>
         </StatusMessageContainer>
     )
 }
