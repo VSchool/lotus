e232c7afda74983d812ce5b10a59aa8ab795fb27
Michael Stedman <mikeybones@rocketmail.com>
2020-07-22 04:02:53 -0500

    Updated Modal.example.js
    Updated layout.

--- example/src/example-components/Modal.example.js
+++ example/src/example-components/Modal.example.js
@@ -1,5 +1,31 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 import { Modal } from '@vschool/lotus'
+
+const ModalExampleContainer = styled.div`
+    /* border: 1px solid lightcoral; */
+`
+
+const SectionName = styled.p`
+    margin: 0px 0px 24px 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 24px;
+    color: #000000;
+`
+
+// const PropDesc = styled.p`
+//     margin: 16px 0px 8px 0px;
+//     font-family: 'aktiv-grotesk';
+//     font-style: normal;
+//     font-weight: 300;
+//     font-size: 16px;
+//     line-height: 19px;
+//     color: rgba(0, 0, 0, 0.6);
+//     box-sizing: border-box;
+// `
 
 const title = 'Are you planning on applying for any V School Scholarships?'
 const subtitle = 'Complete the  Scholarship Application'
@@ -7,9 +33,10 @@ const body = 'If you are interested in applying for scholarships, make sure to a
 
 export default () => {
     return (
-        <Fragment>
-            <br></br>
+        <ModalExampleContainer>
+            <SectionName>Examples</SectionName>
+            {/* <PropDesc>'not-started'</PropDesc> */}
             <Modal title={title} subtitle={subtitle} body={body} />
-        </Fragment>
+        </ModalExampleContainer>
     )
 }
