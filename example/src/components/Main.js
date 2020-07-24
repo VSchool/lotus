4ea9b0a435ee3969e33ccb8ff50bcbfd3066906b
Michael Stedman <mikeybones@rocketmail.com>
2020-07-24 18:30:55 -0500

    Created Template Component
    Created a temporary component to help with implementing React Router.

--- example/src/components/Main.js
+++ example/src/components/Main.js
@@ -4,6 +4,9 @@ import { ComponentDisplayContext } from "../providers/Store"
 import { decideInfo } from "./Main.utils"
 import Table from "./Table"
 
+import Template from './Template'
+
+
 import ButtonExample from '../example-components/Button.example'
 import CardExample from '../example-components/Card.example'
 import ControlsExample from '../example-components/Controls.example'
@@ -24,70 +27,70 @@ const MainContainer = styled.div`
     /* background: #f6e8f9; */
 `
 
-const WelcomeText = styled.p`
-    margin: 0px 0px 16px 0px;
-    font-family: "aktiv-grotesk";
-    font-style: normal;
-    font-weight: 300;
-    font-size: 16px;
-    line-height: 19px;
-    color: rgba(0, 0, 0, 0.6);
-    box-sizing: border-box;
-`
+// const WelcomeText = styled.p`
+//     margin: 0px 0px 16px 0px;
+//     font-family: "aktiv-grotesk";
+//     font-style: normal;
+//     font-weight: 300;
+//     font-size: 16px;
+//     line-height: 19px;
+//     color: rgba(0, 0, 0, 0.6);
+//     box-sizing: border-box;
+// `
 
-const HeaderText = styled.p`
-    margin: 0px 0px 0px 0px;
-    font-family: "aktiv-grotesk";
-    font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 24px;
-    color: #000000;
-`
+// const HeaderText = styled.p`
+//     margin: 0px 0px 0px 0px;
+//     font-family: "aktiv-grotesk";
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 20px;
+//     line-height: 24px;
+//     color: #000000;
+// `
 
-const LineBreak = styled.div`
-    /* width: 80%; */
-    margin: 16px 0px 24px 0px;
-    height: 2px;
-    background: #d1c8c8;
-`
+// const LineBreak = styled.div`
+//     /* width: 80%; */
+//     margin: 16px 0px 24px 0px;
+//     height: 2px;
+//     background: #d1c8c8;
+// `
 
-const IntroBody = styled.p`
-    margin: 0px 0px 16px 0px;
-    /* height: 100px; */
-    font-family: "aktiv-grotesk";
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 16px;
-    color: #000000;
-    opacity: 0.65;
-    /* border: 1px dotted gray; */
-`
+// const IntroBody = styled.p`
+//     margin: 0px 0px 16px 0px;
+//     /* height: 100px; */
+//     font-family: "aktiv-grotesk";
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 14px;
+//     line-height: 16px;
+//     color: #000000;
+//     opacity: 0.65;
+//     /* border: 1px dotted gray; */
+// `
 
-const ComponentName = styled.p`
-    margin: 0px 0px 16px 0px;
-    font-family: "aktiv-grotesk";
-    font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 24px;
-    color: #000000;
-    /* border: 1px solid orange; */
-`
+// const ComponentName = styled.p`
+//     margin: 0px 0px 16px 0px;
+//     font-family: "aktiv-grotesk";
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 20px;
+//     line-height: 24px;
+//     color: #000000;
+//     /* border: 1px solid orange; */
+// `
 
-const BodyText = styled.p`
-    margin: 0px 0px 24px 0px;
-    height: 50px;
-    font-family: "aktiv-grotesk";
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 16px;
-    color: #000000;
-    opacity: 0.65;
-    /* border: 1px solid green; */
-`
+// const BodyText = styled.p`
+//     margin: 0px 0px 24px 0px;
+//     height: 50px;
+//     font-family: "aktiv-grotesk";
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 14px;
+//     line-height: 16px;
+//     color: #000000;
+//     opacity: 0.65;
+//     /* border: 1px solid green; */
+// `
 
 export default function Main() {
     const [componentDisplay] = useContext(ComponentDisplayContext)
@@ -96,51 +99,54 @@ export default function Main() {
     const { compName, description } = newObj
     console.log(compName, description)
 
-    function displayComponent(displayComponentName) {
-        switch (displayComponentName) {
-            case "Header":
-                return <HeaderExample />
-            case "Modal":
-                return <ModalExample />
-            case "Status Message":
-                return <StatusMessageExample />
-            case "Card":
-                return <CardExample />
-            case "Controls":
-                return <ControlsExample />
-            case "Footer":
-                return <FooterExample />
-            case "Profile":
-                return <ProfileExample />
-            case "StatusChip":
-                return <StatusChipExample />
-            case "Taskbar":
-                return <TaskbarExample />
-            case "TitlebarItem":
-                return <TitlebarItemExample />
-            case "Titlebar":
-                return <TitlebarExample />
-            case 'Button':
-                return <ButtonExample />
-            default:
-                return "Default Case"
-        }
-    }
-    let rando = displayComponent(name)
+    // function displayComponent(displayComponentName) {
+    //     switch (displayComponentName) {
+    //         case "Header":
+    //             return <HeaderExample />
+    //         case "Modal":
+    //             return <ModalExample />
+    //         case "Status Message":
+    //             return <StatusMessageExample />
+    //         case "Card":
+    //             return <CardExample />
+    //         case "Controls":
+    //             return <ControlsExample />
+    //         case "Footer":
+    //             return <FooterExample />
+    //         case "Profile":
+    //             return <ProfileExample />
+    //         case "StatusChip":
+    //             return <StatusChipExample />
+    //         case "Taskbar":
+    //             return <TaskbarExample />
+    //         case "TitlebarItem":
+    //             return <TitlebarItemExample />
+    //         case "Titlebar":
+    //             return <TitlebarExample />
+    //         case 'Button':
+    //             return <ButtonExample />
+    //         default:
+    //             return "Default Case"
+    //     }
+    // }
+    // let rando = displayComponent(name)
 
     return (
         <MainContainer>
-            <WelcomeText>Welcome</WelcomeText>
-            <HeaderText>Introduction</HeaderText>
-            <LineBreak />
-            <IntroBody>
-                The following references will provide guidance on how V School components are used.
-            </IntroBody>
-            <ComponentName>{compName}</ComponentName>
-            <BodyText>{description}</BodyText>
-            <Table component={compName} />
-            <LineBreak />
-            {rando}
+            <Template />
+            <Template />
+
+            {/* <WelcomeText>Welcome</WelcomeText> */}
+            {/* <HeaderText>Introduction</HeaderText> */}
+            {/* <LineBreak /> */}
+            {/* <IntroBody> */}
+                {/* The following references will provide guidance on how V School components are used. */}
+            {/* </IntroBody> */}
+            {/* <ComponentName>{compName}</ComponentName> */}
+            {/* <BodyText>{description}</BodyText> */}
+            {/* <Table component={compName} /> */}
+            {/* <LineBreak /> */}
+            {/* {rando} */}
         </MainContainer>
     )
 }
