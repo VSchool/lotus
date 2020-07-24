a0b4dbcee4b23d3ff22ac99c93bc518f98821f08
Michael Stedman <mikeybones@rocketmail.com>
2020-07-24 12:15:53 -0500

    Updated TitlebarItem utils.js
    Used universal deconstructor for colors, and replaced colors with new values.

--- src/components/TitlebarItem/utils.js
+++ src/components/TitlebarItem/utils.js
@@ -1,30 +1,29 @@
-import { gray } from "../../colors/gray"
-import { green } from "../../colors/green"
+import * as colors from '../../colors'
 
 export function itemStatus(itemStatus) {
     switch (itemStatus) {
         case "active":
             return {
-                border: `4px solid ${green.lighter}`,
+                border: `4px solid ${colors.green.lighter}`,
                 opacity: "1.0",
-                titleColor: gray.base,
-                subtitleColor: gray.lighter,
+                titleColor: colors.gray.base,
+                subtitleColor: colors.gray.lighter,
                 checkmarkDisplay: "none"
             }
         case "inactive":
             return {
                 border: "none",
                 opacity: "1.0",
-                titleColor: gray.base,
-                subtitleColor: gray.lighter,
+                titleColor: colors.gray.base,
+                subtitleColor: colors.gray.lighter,
                 checkmarkDisplay: "inline-block"
             }
         case "locked":
             return {
                 border: "none",
                 opacity: "0.67",
-                titleColor: gray.base,
-                subtitleColor: gray.lighter,
+                titleColor: colors.gray.base,
+                subtitleColor: colors.gray.lighter,
                 checkmarkDisplay: "none"
             }
 
