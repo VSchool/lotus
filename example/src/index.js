c7f151751bd2f0eb6d0d61f203295e602315d358
Michael Stedman <mikeybones@rocketmail.com>
2020-07-24 18:50:46 -0500

    Updated Index.js
    Removed everything context related.

--- example/src/index.js
+++ example/src/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Store from "./providers/Store"
 import App from "./App"
 import "./index.css"
 
 function Index() {
     return (
-        <Store>
+        
             <App />
-        </Store>
+        
     )
 }
 
