43dc6d7ee6126efe9fa008890433986f706c1b07
Michael Stedman <mikeybones@rocketmail.com>
2020-07-24 19:01:23 -0500

    Updated Index.js
    Installed react-router-dom, and put BrowserRouter in Index.js.

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter } from 'react-router-dom'
 import App from "./App"
 import "./index.css"
 
 function Index() {
     return (
-        
+        <BrowserRouter>
             <App />
-        
+        </BrowserRouter>
     )
 }
 
