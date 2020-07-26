54b7f040400f5c3eeb3af1a4a89f9b5c305cdc25
Michael Stedman <mikeybones@rocketmail.com>
2020-07-26 17:42:46 -0500

    Updated ComponetDocTemplate.js
    Commit

---
+++ example/src/components/ComponentDocTemplate.js
@@ -0,0 +1,199 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import copyLink from '../assets/images/link.svg'
+
+const DocTemplateContainer = styled.div`
+    /* border: 1px solid lightcoral; */
+`
+
+const TemplateHeaderContainer = styled.div`
+    margin: 0px 0px 0px 0px;
+    width: 100%;
+    display: flex;
+    /* border: 1px solid blue; */
+`
+
+const TemplateTitle = styled.h3`
+    margin: 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 56px;
+    line-height: 64px;
+    color: #000000;
+    /* border: 1px dotted gray; */
+`
+
+const CopyLinkContainer = styled.div`
+    margin: 0px 0px 0px 16px;
+    width: 24px;
+    height: 24px;
+    /* border: 1px dotted green; */
+`
+
+const TemplateDescriptionContainer = styled.div`
+    margin: 16px 0px 0px 0px;
+    /* border: 1px dashed green; */
+`
+
+const TemplateDescriptionText = styled.p`
+    margin: 0px 0px 0px 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    /* border: 1px solid gray; */
+`
+
+const TemplateExampleContainer = styled.div`
+    margin: 56px 0px 0px 0px;
+    /* width: 800px; */
+    /* height: 56px; */
+    padding: 8px 16px 8px 16px;
+    background: #F4F2F2;
+    border-left: 4px solid #59AF9B;
+
+    /* border: 1px solid gold; */
+`
+
+const TemplateExampleText = styled.p`
+    margin: 0px;
+
+    font-family: Source Code Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+    color: #000000;
+    /* border: 1px solid orange; */
+`
+
+const TemplatePropsContainer = styled.div`
+    margin: 56px 0px 0px 0px;
+    /* border: 1px solid lightcoral; */
+`
+
+const PropsHeaderText = styled.h3`
+    margin: 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 32px;
+    line-height: 40px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+    /* border: 1px dotted gray; */
+`
+
+const PropsInfoContainer = styled.div`
+    margin: 40px 0px 0px 0px;
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* border: 1px solid lightblue; */
+`
+
+const InfoWrapper = styled.div`
+    margin: 0px 0px 8px 0px;
+    display: flex;
+    /* border: 1px solid purple; */
+`
+
+const InfoBold = styled.p`
+    margin: 0px 0px 0px 0px;
+    width: 80px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+    /* border: 1px dashed gray; */
+`
+
+const InfoRegular = styled.p`
+    margin: 0px 0px 0px 0px;
+    /* width: 150px; */
+    font-family: 'aktiv-grotesk';
+    font-style: italic;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+    /* border: 1px dashed red; */
+`
+
+const InfoDescription = styled.p`
+    margin: 0px;
+    font-family: 'aktiv-grotesk';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    color: #000000;
+    /* border: 1px dotted black; */
+`
+
+
+
+function ComopnentDocTemplate(props) {
+    const { componentName, componentDescription, exampleText, templateProps } = props.templateInfo
+
+    let newArr = templateProps.map((availableProp, idx) => {
+        const { propName, propType, propDescription, options } = availableProp
+        let optionListString = options.map((option) => {
+            // console.log(`Hello ${option}`)
+            return `${option}, `
+        })
+        console.log(optionListString)
+
+
+        return (
+            <PropsInfoContainer key={idx}>
+                <InfoWrapper>
+                    <InfoBold>prop:</InfoBold><InfoRegular>{propName}</InfoRegular>
+                </InfoWrapper>
+                <InfoWrapper>
+                    <InfoBold>type:</InfoBold><InfoRegular>{propType}</InfoRegular>
+                </InfoWrapper>
+                <InfoWrapper>
+                    <InfoBold>options:</InfoBold><InfoRegular>{optionListString}</InfoRegular>
+                </InfoWrapper>
+                <InfoDescription>{propDescription}</InfoDescription>
+            </PropsInfoContainer>
+        )
+    })
+
+    // console.log(templateProps)
+    return (
+        <DocTemplateContainer>
+            <TemplateHeaderContainer>
+                <TemplateTitle>{componentName}</TemplateTitle>
+                <CopyLinkContainer><img src={copyLink} alt={'Click to copy URL for this component.'} /></CopyLinkContainer>
+            </TemplateHeaderContainer>
+            <TemplateDescriptionContainer>
+                <TemplateDescriptionText>{componentDescription}</TemplateDescriptionText>
+            </TemplateDescriptionContainer>
+            <TemplateExampleContainer>
+                <TemplateExampleText>{exampleText}</TemplateExampleText>
+            </TemplateExampleContainer>
+            <TemplatePropsContainer>
+                <PropsHeaderText>Props</PropsHeaderText>
+            </TemplatePropsContainer>
+            {newArr}
+            {/* <PropsInfoContainer>
+                <InfoBold>prop:</InfoBold><InfoRegular>title</InfoRegular>
+            </PropsInfoContainer> */}
+        </DocTemplateContainer>
+    )
+}
+
+export default ComopnentDocTemplate
--- example/src/example-components/Card.example.js
+++ example/src/example-components/Card.example.js
@@ -1,177 +1,217 @@
 import React from "react"
 import styled from "styled-components"
-import { useLocation } from 'react-router-dom'
-import { Card } from "@vschool/lotus"
-import clipboardCopyLink from '../assets/images/link.svg'
+// import { useLocation } from 'react-router-dom'
+import ComponentDocTemplate from '../components/ComponentDocTemplate'
+// import { Card } from "@vschool/lotus"
+// import clipboardCopyLink from '../assets/images/link.svg'
+
+
+const CardTemplateObj = {
+    componentName: 'Modal',
+    componentDescription: 'Here is a short description for this component to go into the template.',
+    exampleText: `<Modal title={string} subtitle={string} body={string} />`,
+    templateProps: [
+        {
+            propName: 'title',
+            propType: 'String',
+            propDescription: 'Here is another area for a description that I need to input a placeholder for.',
+            options: ['accepts a String']
+        },
+        {
+            propName: 'subtitle',
+            propType: 'String',
+            propDescription: 'Here is another area for a description that I need to input a placeholder for.',
+            options: ['accepts a String']
+        },
+        {
+            propName: 'body',
+            propType: 'String',
+            propDescription: 'Here is another area for a description that I need to input a placeholder for.',
+            options: ['accepts a String']
+        }
+    ],
+    reactCompnent: ''
+}
 
 const CardExampleContainer = styled.div`
     /* border: 1px solid lightcoral; */
 `
 
-const CardPageHeaderContainer = styled.div`
-    display: flex;
-`
 
-const CardPageTitle = styled.p`
-    margin: 0px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 64px;
-    line-height: 75px;
-    color: #000000;
-`
 
-const ClipboardCopyLinkContainer = styled.div`
-    margin: 0px 0px 0px 16px;
-    width: 24px;
-    height: 24px;
-`
 
-const ConfirmCopied = styled.p`
-    margin: 0px 0px 0px 8px;
-    display: none;
-    align-items: center;
-    width: 100%;
-    height: 24px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 24px;
-    line-height: 24px;
-    color: #000000;
-    opacity: .5;
-`
 
-const CardPageDescription = styled.p`
-    margin: 24px 0px 0px 0px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 24px;
-    color: #000000;
-`
+// const CardPageHeaderContainer = styled.div`
+//     display: flex;
+// `
 
-const CardPageSubHeader = styled.p`
-    margin: 40px 0px 0px 0px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 32px;
-    line-height: 40px;
-    display: flex;
-    align-items: center;
-    color: #000000;
-`
+// const CardPageTitle = styled.p`
+//     margin: 0px;
+//     font-family: Roboto;
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 64px;
+//     line-height: 75px;
+//     color: #000000;
+// `
 
-const DisplayContainer = styled.div`
-    margin: 32px 0px 0px 0px;
-`
+// const ClipboardCopyLinkContainer = styled.div`
+//     margin: 0px 0px 0px 16px;
+//     width: 24px;
+//     height: 24px;
+// `
 
-const PropNameText = styled.p`
-    margin: 0px;
-    font-family: Roboto;
-    font-style: italic;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 24px;
-    display: flex;
-    align-items: center;
-    color: #000000;
-` 
+// const ConfirmCopied = styled.p`
+//     margin: 0px 0px 0px 8px;
+//     display: none;
+//     align-items: center;
+//     width: 100%;
+//     height: 24px;
+//     font-family: Roboto;
+//     font-style: normal;
+//     font-weight: 300;
+//     font-size: 24px;
+//     line-height: 24px;
+//     color: #000000;
+//     opacity: .5;
+// `
 
-const PropTypeText = styled.p`
-    margin: 8px 0px 8px 0px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 24px;
-    display: flex;
-    align-items: center;
-    color: #000000;
-`
+// const CardPageDescription = styled.p`
+//     margin: 24px 0px 0px 0px;
+//     font-family: Roboto;
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 20px;
+//     line-height: 24px;
+//     color: #000000;
+// `
 
-const PropDescriptionText = styled.p`
-    margin: 0px 0px 0px 0px;
-    font-family: Roboto;
-    font-style: italic;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 24px;
-    display: flex;
-    align-items: center;
-    color: #000000;
-`
+// const CardPageSubHeader = styled.p`
+//     margin: 40px 0px 0px 0px;
+//     font-family: Roboto;
+//     font-style: normal;
+//     font-weight: 500;
+//     font-size: 32px;
+//     line-height: 40px;
+//     display: flex;
+//     align-items: center;
+//     color: #000000;
+// `
 
-const PropDemoContainer = styled.div`
-    margin: 16px 0px 0px 0px;
-    padding: 8px 16px 8px 16px;
-    background: rgba(196, 196, 196, 0.25);
-    border-radius: 8px;
-`
+// const DisplayContainer = styled.div`
+//     margin: 32px 0px 0px 0px;
+// `
 
-const PropDemoText = styled.p`
-    margin: 0px;
-    font-family: Source Code Pro;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 16px;
-    line-height: 24px;
-    display: flex;
-    align-items: center;
-    color: #000000;
-`
+// const PropNameText = styled.p`
+//     margin: 0px;
+//     font-family: Roboto;
+//     font-style: italic;
+//     font-weight: 300;
+//     font-size: 18px;
+//     line-height: 24px;
+//     display: flex;
+//     align-items: center;
+//     color: #000000;
+// ` 
 
-const ExampleContainer = styled.div`
-    margin: 16px 0px 0px 0px;
-    padding: 8px 0px 8px 0px;
-`
+// const PropTypeText = styled.p`
+//     margin: 8px 0px 8px 0px;
+//     font-family: Roboto;
+//     font-style: normal;
+//     font-weight: 300;
+//     font-size: 18px;
+//     line-height: 24px;
+//     display: flex;
+//     align-items: center;
+//     color: #000000;
+// `
 
-const ExampleText = styled.p`
-    margin: 24px 0px 8px 0px;
-`
+// const PropDescriptionText = styled.p`
+//     margin: 0px 0px 0px 0px;
+//     font-family: Roboto;
+//     font-style: italic;
+//     font-weight: 300;
+//     font-size: 18px;
+//     line-height: 24px;
+//     display: flex;
+//     align-items: center;
+//     color: #000000;
+// `
+
+// const PropDemoContainer = styled.div`
+//     margin: 16px 0px 0px 0px;
+//     padding: 8px 16px 8px 16px;
+//     background: rgba(196, 196, 196, 0.25);
+//     border-radius: 8px;
+// `
+
+// const PropDemoText = styled.p`
+//     margin: 0px;
+//     font-family: Source Code Pro;
+//     font-style: normal;
+//     font-weight: 300;
+//     font-size: 16px;
+//     line-height: 24px;
+//     display: flex;
+//     align-items: center;
+//     color: #000000;
+// `
+
+// const ExampleContainer = styled.div`
+//     margin: 16px 0px 0px 0px;
+//     padding: 8px 0px 8px 0px;
+// `
+
+// const ExampleText = styled.p`
+//     margin: 24px 0px 8px 0px;
+// `
+
+// console.log(CardTemplateObj)
+
 
 export default () => {
-    let location = useLocation()
+    // let location = useLocation()
 
-    function copyToClipboard() {
-        let dummyElement = document.createElement('input')
-        let tempHostName = 'http://localhost:3000'
-        let text = `${tempHostName}${location.pathname}`
+    // function copyToClipboard() {
+    //     let dummyElement = document.createElement('input')
+    //     let tempHostName = 'http://localhost:3000'
+    //     let text = `${tempHostName}${location.pathname}`
 
-        document.body.appendChild(dummyElement)
-        dummyElement.value = text
-        dummyElement.select()
-        document.execCommand('copy')
-        document.body.removeChild(dummyElement)
+    //     document.body.appendChild(dummyElement)
+    //     dummyElement.value = text
+    //     dummyElement.select()
+    //     document.execCommand('copy')
+    //     document.body.removeChild(dummyElement)
 
-        let confirmCopy = document.getElementById('confirmCopy')
-        confirmCopy.style.display = 'flex'
-    }
+    //     let confirmCopy = document.getElementById('confirmCopy')
+    //     confirmCopy.style.display = 'flex'
+    // }
 
     return (
         <CardExampleContainer>
-            <CardPageHeaderContainer>
+            <ComponentDocTemplate templateInfo={CardTemplateObj} />
+
+            {/* <CardPageHeaderContainer>
                 <CardPageTitle>Card</CardPageTitle>
                 <ClipboardCopyLinkContainer onClick={copyToClipboard}>
                     <img src={clipboardCopyLink} alt={'Click to copy url'} />
                 </ClipboardCopyLinkContainer>
                 <ConfirmCopied id={'confirmCopy'}>Copied to clipboard!</ConfirmCopied>
             </CardPageHeaderContainer>
-            <CardPageDescription>A brief description of what the component is used for.</CardPageDescription>
+            <CardPageDescription>A brief description of what the component is used for.</CardPageDescription> */}
             
-            <DisplayContainer>
+            {/* <DisplayContainer>
                 <PropDemoContainer>
                     <PropDemoText>{'<Card status={\'completed\'} />'}</PropDemoText>
                 </PropDemoContainer>
                 <ExampleContainer>
                     <Card status={'completed'} />
                 </ExampleContainer>
-            </DisplayContainer>
-            <CardPageSubHeader>Props</CardPageSubHeader>
-            <DisplayContainer>
+            </DisplayContainer> */}
+
+            {/* <CardPageSubHeader>Props</CardPageSubHeader> */}
+
+            {/* <DisplayContainer>
                 <PropNameText>status</PropNameText>
                 <PropTypeText>type: String</PropTypeText>
                 <PropDescriptionText>'not-started', 'up-next', 'in-progress', 'completed'</PropDescriptionText>
@@ -191,7 +231,7 @@ export default () => {
                     <ExampleText>{"'status={'completed'}'"}</ExampleText>
                     <Card status={'completed'} />
                 </ExampleContainer>
-            </DisplayContainer>
+            </DisplayContainer> */}
         </CardExampleContainer>
     )
 }
