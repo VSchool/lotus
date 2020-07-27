2afce7451937f4b4f7044504a01864c9d5c71c27
Michael Stedman <mikeybones@rocketmail.com>
2020-07-26 20:25:04 -0500

    Other thing functional
    Other thing is functional across all components.

--- example/src/components/LiveDemo.js
+++ example/src/components/LiveDemo.js
@@ -7,13 +7,37 @@ import {
     LivePreview
 } from 'react-live'
 
-import { Controls, Card, Modal} from '@vschool/lotus'
+import { 
+    Controls,
+    Card,
+    Footer,
+    Header,
+    Modal,
+    Profile,
+    StatusChip,
+    StatusMessage,
+    Taskbar,
+    TitlebarItem,
+    Titlebar
+} from '@vschool/lotus'
 
 const LiveDemoContainer = styled.div`
     /* background: lightgreen; */
 `
 
-const scope = {Controls, Card, Modal}
+const scope = {
+    Controls,
+    Card,
+    Footer,
+    Header,
+    Modal,
+    Profile,
+    StatusChip,
+    StatusMessage,
+    Taskbar,
+    TitlebarItem,
+    Titlebar
+}
 
 function createCode(name) {
     switch (name) {
@@ -25,9 +49,41 @@ function createCode(name) {
             return (
                 `<Card status={'up-next'} />`
             )
+        case 'Footer':
+            return (
+                `<Footer />`
+            )
+        case 'Header':
+            return (
+                `<Header />`
+            )
         case 'Modal':
             return (
                 `<Modal title={'Hello'} subtitle={'World'} body={'body'} />`
+            )
+        case 'Profile':
+            return (
+                `<Profile userName={'Namey Namerson'} userEmail={'nameynamerson@coolemail.com'} userPhone={'999-999-9999'} userCourse={'Fullstack Javascript'} userTime={'Full - Time'} />`
+            )
+        case 'Status Chip':
+            return (
+                `<StatusChip status={'not-started'} />`
+            )
+        case 'StatusMessage':
+            return (
+                `<StatusMessage status={'neutral'} message={'Welcome to your V School Application. Get started by completing the tasks below!'} />`
+            )
+        case 'Taskbar':
+            return (
+                `<Taskbar title={'Required'} task={'FSJS - Aptitude Test'} />`
+            )
+        case 'Titlebar Item':
+            return (
+                `<TitlebarItem status={'active'} title={'Step 1'} subtitle={'Apply'} />`
+            )
+        case 'Titlebar':
+            return (
+                `<Titlebar stepOne={'active'} stepTwo={'inactive'} stepThree={'locked'} />`
             )
         default:
             return `There is no spoon.`
