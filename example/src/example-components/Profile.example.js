cea1f0aaacbfabca48eeb6408efe9387c570a414
Michael Stedman <mikeybones@rocketmail.com>
2020-07-27 17:35:42 -0500

    Updated Profile.example.js
    Added component and prop descriptions to be displayed.

--- example/src/example-components/Profile.example.js
+++ example/src/example-components/Profile.example.js
@@ -5,37 +5,37 @@ import ComponentDocTemplate from '../components/ComponentDocTemplate'
 
 const profileTemplateObj = {
     componentName: 'Profile',
-    componentDescription: 'Here is a short description for this component to go into the template.',
+    componentDescription: 'Display the user\'s profile information.',
     exampleText: `<Profile userName={string} userEmail={string} userPhone={string} userCourse={string} userTime={string} />`,
     templateProps: [
         {
             propName: 'userName',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that displays the user\'s name.',
             options: ['accepts any String']
         },
         {
             propName: 'userEmail',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that displays the user\'s email address.',
             options: ['accepts any String']
         },
         {
             propName: 'userPhone',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that displays the user\'s phone number.',
             options: ['accepts any String']
         },
         {
             propName: 'userCourse',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that displays the user\'s course they are enrolled in.',
             options: ['accepts any String']
         },
         {
             propName: 'userTime',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that displays the user\'s time committment level.',
             options: ['accepts any String']
         }
     ],
