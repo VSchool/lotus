5810d5f33e7588002e8efed623a8da8d9cadd577
Michael Stedman <mikeybones@rocketmail.com>
2020-07-27 17:44:59 -0500

    Updated Taskbar.example.js
    Added component and prop descriptions to be displayed.

--- example/src/example-components/Taskbar.example.js
+++ example/src/example-components/Taskbar.example.js
@@ -5,19 +5,19 @@ import ComponentDocTemplate from '../components/ComponentDocTemplate'
 
 const TaskbarTemplateObj = {
     componentName: 'Taskbar',
-    componentDescription: 'Here is a short description for this component to go into the template.',
+    componentDescription: 'Provide users with a way to know what to do.',
     exampleText: `<Taskbar title={string} task={string} />`,
     templateProps: [
         {
             propName: 'title',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that is displayed in the title of the taskbar.',
             options: ['accepts any String']
         },
         {
             propName: 'task',
             propType: 'String',
-            propDescription: 'Status Here is another area for a description that I need to input a placeholder for.',
+            propDescription: 'Text string that is displayed in the task area of the taskbar.',
             options: ['accepts any String']
         }
     ],
