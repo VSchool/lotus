5137e1d7c91ecd2afdcc8db22ad7dbf2860d4fb3
Michael Stedman <michaelstedman81@gmail.com>
2020-07-28 15:45:55 -0500

    Updated Header.js
    Re-styled component, added pros, and some functionality.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,22 @@
 import React from "react"
 import styled from "styled-components"
-
+import Profile from '../Profile/Profile'
+import { handleClearClick, handleLoggedInClick } from './utils'
+import * as colors from "../../colors"
 import headerLogo from "../../assets/vs-header-logo.svg"
 import backArrow from "../../assets/icons/arrow-back.svg"
 import accountCircle from "../../assets/icons/account-circle24px.svg"
 import clearX from '../../assets/icons/clear24px.svg'
-import * as colors from "../../colors"
-
-import Profile from '../Profile/Profile'
 
 const HeaderContainer = styled.div`
     position: relative;
     max-width: 100%;
-    /* height: 80px; */
     padding: 28px 24px 28px 24px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     background: ${colors.gray.lighter};
     box-shadow: 0px 4px 16px rgba(33, 32, 31, 0.1);
-    /* border: 1px solid lightcoral; */
 
     @media (min-width: 768px) {
         max-width: 100%;
@@ -29,7 +26,6 @@ const HeaderContainer = styled.div`
 const BackContainer = styled.div`
     display: flex;
     align-items: center;
-    /* border: 1px solid lightcoral; */
 `
 
 const BackArrowContainer = styled.div`
@@ -39,7 +35,6 @@ const BackArrowContainer = styled.div`
     justify-content: center;
     align-items: center;
     box-sizing: border-box;
-    /* border: 1px dotted gray; */
 `
 
 const BackText = styled.p`
@@ -50,8 +45,7 @@ const BackText = styled.p`
     font-weight: 500;
     font-size: 20px;
     line-height: 24px;
-    color: #514F4B;
-    /* border: 1px dotted blue; */
+    color: ${colors.gray.darker};
 
     @media (min-width: 768px) {
         display: block;
@@ -61,7 +55,6 @@ const BackText = styled.p`
 const HeaderLogoContainer = styled.div`
     display: flex;
     align-items: center;
-    /* border: 1px solid blue; */
 `
 
 const IconContainer = styled.div`
@@ -70,7 +63,6 @@ const IconContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    /* border: 1px dotted lightcoral; */
 `
 
 const EmptyDiv = styled.div`
@@ -82,37 +74,22 @@ const ProfileElementContainer = styled.div`
     position: absolute;
     top: 88px;
     right: 24px;
-    /* border: 1px solid blue; */
 `
 
+function chooseIcon(iconState) {
+    switch (iconState) {
+        case 'default':
+            return <EmptyDiv></EmptyDiv>
+        case 'loggedIn':
+            return <img src={accountCircle} alt='hello' onClick={handleLoggedInClick} />
+        case 'closeProfile':
+            return <img src={clearX} alt='hello' onClick={handleClearClick} />
+        default:
+            return ''
+    }
+}
+
 function Header(props) {
-
-    function chooseIcon(iconState) {
-        switch (iconState) {
-            case 'default':
-                return<EmptyDiv></EmptyDiv>
-            case 'loggedIn':
-                return<img src={accountCircle} alt='hello' onClick={handleLoggedInClick} />
-            case 'closeProfile':
-                return<img src={clearX} alt='hello' onClick={handleClearClick} />
-            default:
-                return ''
-        }
-    }
-
-    function handleClearClick() {
-        console.log('ClearClick Fired')
-    }
-
-    function handleLoggedInClick() {
-        let profile = document.getElementById('profileElement')
-        if (profile.style.display === '') {
-            profile.style.display = 'block'
-        } else if (profile.style.display === 'block') {
-            profile.style.display = ''
-        }
-    }
-
     let IconToDisplay = chooseIcon(props.headerState)
 
     return (
