b916ed38289f47c1e05e9c48dd8bf3108912aaa7
Michael Stedman <michaelstedman81@gmail.com>
2020-08-05 14:22:38 -0500

    Updated ComponentDocTemplate Component
    Clipboard confirmation is now disappearing after setTimeout.

--- example/src/components/ComponentDocTemplate.js
+++ example/src/components/ComponentDocTemplate.js
@@ -51,100 +51,6 @@ const TemplateDescriptionText = styled.p`
     /* border: 1px solid gray; */
 `
 
-// const TemplateExampleContainer = styled.div`
-//     margin: 16px 0px 0px 0px;
-//     max-width: 768px;
-//     /* height: 56px; */
-//     padding: 8px 16px 8px 16px;
-//     background: #f4f2f2;
-//     border-left: 4px solid #59af9b;
-//     /* border: 1px solid gold; */
-// `
-
-// const TemplateExampleText = styled.p`
-//     margin: 0px;
-//     width: 100%;
-//     font-family: Source Code Pro;
-//     font-style: normal;
-//     font-weight: normal;
-//     font-size: 16px;
-//     line-height: 24px;
-//     color: #000000;
-//     /* border: 1px solid orange; */
-// `
-
-// const TemplatePropsContainer = styled.div`
-//     margin: 56px 0px 0px 0px;
-//     /* border: 1px solid lightcoral; */
-// `
-
-// const PropsHeaderText = styled.h3`
-//     margin: 0px;
-//     font-family: "aktiv-grotesk";
-//     font-style: normal;
-//     font-weight: normal;
-//     font-size: 32px;
-//     line-height: 40px;
-//     display: flex;
-//     align-items: center;
-//     color: #000000;
-//     /* border: 1px dotted gray; */
-// `
-
-// const PropsInfoContainer = styled.div`
-//     margin: 40px 0px 0px 0px;
-//     /* border: 1px solid lightblue; */
-// `
-
-// const InfoWrapper = styled.div`
-//     margin: 0px 0px 0px 0px;
-//     display: flex;
-//     /* border: 1px solid purple; */
-// `
-
-// const InfoBold = styled.p`
-//     margin: 0px 0px 0px 0px;
-//     width: 96px;
-//     font-family: "aktiv-grotesk";
-//     font-style: normal;
-//     font-weight: medium;
-//     font-size: 20px;
-//     line-height: 24px;
-//     /* display: flex; */
-//     /* align-items: center; */
-//     color: #000000;
-//     /* border: 1px dashed gray; */
-// `
-
-// const InfoRegular = styled.p`
-//     margin: 0px 0px 0px 0px;
-//     width: 100%;
-//     max-height: 56px;
-//     /* width: 150px; */
-//     font-family: "aktiv-grotesk";
-//     font-style: italic;
-//     font-weight: lighter;
-//     font-size: 20px;
-//     line-height: 24px;
-//     /* display: flex; */
-//     /* align-items: center; */
-//     color: #000000;
-//     /* border: 1px dashed red; */
-// `
-
-// const InfoDescription = styled.p`
-//     margin: 0px;
-//     font-family: "aktiv-grotesk";
-//     font-style: normal;
-//     font-weight: 300;
-//     font-size: 20px;
-//     line-height: 24px;
-//     display: flex;
-//     align-items: center;
-//     color: #000000;
-//     /* border: 1px dotted black; */
-// `
-
 const HiddenParagraph = styled.p`
     margin: 0px 0px 0px 8px;
     height: 24px;
@@ -175,35 +81,14 @@ function ComopnentDocTemplate(props) {
         document.execCommand("copy")
         document.body.removeChild(dummyElement)
 
+        function hideDiv() {
+            copiedConfirmation.style.display = 'none'
+        }
         copiedConfirmation.style.display = 'flex'
+        setTimeout(hideDiv, 1500)
     }
 
     const { componentName, componentDescription } = props.templateInfo
-    // templateProps & exampleText
-
-    // let newArr = templateProps.map((availableProp, idx) => {
-    //     const { propName, propType, propDescription, options } = availableProp
-    //     let optionListString = options.map(option => {
-    //         return `${option}, `
-    //     })
-    //     return (
-    //         <PropsInfoContainer key={idx}>
-    //             <InfoWrapper>
-    //                 <InfoBold>prop:</InfoBold>
-    //                 <InfoRegular>{propName}</InfoRegular>
-    //             </InfoWrapper>
-    //             <InfoWrapper>
-    //                 <InfoBold>type:</InfoBold>
-    //                 <InfoRegular>{propType}</InfoRegular>
-    //             </InfoWrapper>
-    //             <InfoWrapper>
-    //                 <InfoBold>options:</InfoBold>
-    //                 <InfoRegular>{optionListString}</InfoRegular>
-    //             </InfoWrapper>
-    //             <InfoDescription>{propDescription}</InfoDescription>
-    //         </PropsInfoContainer>
-    //     )
-    // })
 
     return (
         <DocTemplateContainer>
@@ -219,13 +104,6 @@ function ComopnentDocTemplate(props) {
                 <TemplateDescriptionText>{componentDescription}</TemplateDescriptionText>
             </TemplateDescriptionContainer>
             <Table data={props.templateInfo} />
-            {/* <TemplateExampleContainer>
-                <TemplateExampleText>{exampleText}</TemplateExampleText>
-            </TemplateExampleContainer> */}
-            {/* <TemplatePropsContainer>
-                <PropsHeaderText>Props</PropsHeaderText>
-            </TemplatePropsContainer> */}
-            {/* {newArr} */}
             <LiveDemo codeDisplay={props.templateInfo} />
         </DocTemplateContainer>
     )
