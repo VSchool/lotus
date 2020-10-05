bb06d687df7e9626883d22b059c33e942ea958e4
Bob Ziroll <elderziroll@gmail.com>
2020-10-05 12:18:59 -0600

    Complete overhaul - restarted using create-react-library and imported all storybook items

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { Button } from "@vschool/lotus"
+import "@vschool/lotus/dist/index.css"
+
+const App = () => {
+    return <Button>Button text</Button>
+}
+
+export default App
