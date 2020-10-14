b840b5c0a22b35261926aca69e5569589f4ea162
Bob Ziroll <elderziroll@gmail.com>
2020-10-14 10:52:42 -0600

    Reverting back

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,34 @@
 import image from "@rollup/plugin-image"
+// import peerDepsExternal from "rollup-plugin-peer-deps-external"
+// import resolve from "@rollup/plugin-node-resolve"
+// import commonjs from "@rollup/plugin-commonjs"
+// import postcss from "rollup-plugin-postcss"
+// import babel from "rollup-plugin-babel"
+
+import pkg from "./package.json"
 
 export default {
-    plugins: [image()],
+    input: pkg.source,
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: "esm",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        // peerDepsExternal(),
+        // babel({
+        //     exclude: "node_modules/**",
+        // }),
+        // resolve(),
+        // commonjs(),
+        // postcss(),
+        image(),
+    ],
 }
